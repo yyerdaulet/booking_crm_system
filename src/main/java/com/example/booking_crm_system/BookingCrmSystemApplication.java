a7370f8448885375653b5a2b44803cdf38a27757
Yerdaulet <yerdauletacc@gmail.com>
2026-05-26 21:13:11 +0500

    Initial commit

---
+++ src/main/java/com/example/booking_crm_system/BookingCrmSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.booking_crm_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingCrmSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingCrmSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/booking_crm_system/BookingCrmSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.booking_crm_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingCrmSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
